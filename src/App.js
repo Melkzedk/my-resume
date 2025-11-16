49ac5ea590c793bd8c7faf61f782f3c1c860a8cb
melkzedek wafula <melkywafula29@gmail.com>
2025-11-16 11:47:04 +0300

    updated Header.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 function App() {
+  const [dark, setDark] = useState(false);
+
+  const toggleTheme = () => setDark(!dark);
+
   return (
-    <>
-      <Header />
+    <div className={dark ? "bg-dark text-white" : "bg-light text-dark"}>
+      <Header dark={dark} toggleTheme={toggleTheme} />
       <About />
       <Skills />
       <Experience />
-    </>
+      <Contact />
+    </div>
   );
 }
 
