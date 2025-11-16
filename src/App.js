a7649621b5d5aa6a5fb8e276cec951dedc0f7f19
melkzedek wafula <melkywafula29@gmail.com>
2025-11-16 11:43:15 +0300

    updated app.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Skills />
+      <Experience />
+    </>
+  );
+}
+
+export default App;
