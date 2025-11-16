38bb423d49190663f32c7215ed002465a19d08e6
melkzedek wafula <melkywafula29@gmail.com>
2025-11-16 11:40:15 +0300

    updated Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-dark text-white text-center p-4">
+      <h1>John Doe</h1>
+      <p className="lead">Full Stack Developer</p>
+    </header>
+  );
+}
