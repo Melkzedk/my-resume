be5a9c905a4a1b707e0ed35600bb497be379c07d
melkzedek wafula <melkywafula29@gmail.com>
2025-11-16 11:41:23 +0300

    updated Header.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+export default function About() {
+    return (
+        <section className="container my-5">
+            <h2>About Me</h2>
+            <p>
+                I am a passionate Full Stack Developer with experience in building web applications using modern technologies. I enjoy solving complex problems and continuously learning new skills to enhance my development capabilities.
+            </p>
+        </section>
+    );
+}
