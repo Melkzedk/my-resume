8017fcc879c743fe9890eb8bb4e078ab00fb769b
melkzedek wafula <melkywafula29@gmail.com>
2025-11-17 09:53:24 +0300

    small changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -13,7 +13,7 @@ export default function Experience() {
 
       <div className="card mb-3">
         <div className="card-body">
-          <h5 className="card-title">Full Stack Intern – Your Brand Mate</h5>
+          <h5 className="card-title">Full Stack Intern</h5>
           <p className="card-text">React • Next.js • Node.js</p>
           <ul>
             <li>Developed ecommerce system</li>
