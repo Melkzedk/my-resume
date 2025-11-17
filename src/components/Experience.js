091ed0f360e577f989995f66018fd3d1f0701e93
melkzedek wafula <melkywafula29@gmail.com>
2025-11-17 12:10:05 +0300

    small changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -15,7 +15,7 @@ export default function Experience() {
       <div className="card mb-3">
         <div className="card-body">
           <h5 className="card-title">Full Stack Developer(FreeLancing)</h5>
-          <p className="card-text">React • Next.js • Node.js • MongoDb</p>
+          <p className="card-text">React • Next.js • Node.js • MongoDb • Boostrap/tailwindcss</p>
           <ul>
             <li>Developed an ecommerce system</li>
             <li>Built a carpooling system</li>
