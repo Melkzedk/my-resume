cef8a0b94b9422249d7f1f380cfd9c05848132df
melkzedek wafula <melkywafula29@gmail.com>
2026-01-27 09:19:11 +0300

    more changes

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-  //catch(err) {
+  catch(err) {
     //console.error('Test failed:', err);
 //}
 });
